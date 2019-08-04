6cf0cb5e9c338a03164f9ade7a3ac18e3fc231ab
wjj <wangjiajie0326@163.com>
2019-08-04 19:08:56 +0800

    多相逻辑回归类

--- conv_classes.py
+++ conv_classes.py
@@ -183,3 +183,59 @@ class MLP(object):
         self.errors = self.logRegressionLayer.errors
 
         self.params = self.hiddenLayer.params + self.logRegressionLayer.params
+
+
+class LogisticRegression(object):
+
+
+    def __init__(self, input, n_in, n_out, W=None, b=None):
+        """ Initialize the parameters of the logistic regression
+    :type input: theano.tensor.TensorType
+    :param input: symbolic variable that describes the input of the
+    architecture (one minibatch)
+
+    :type n_in: int
+    :param n_in: number of input units, the dimension of the space in
+    which the datapoints lie
+
+    :type n_out: int
+    :param n_out: number of output units, the dimension of the space in
+    which the labels lie
+
+    """
+
+        if W is None:
+            self.W = theano.shared(
+                value=numpy.zeros((n_in, n_out), dtype=theano.config.floatX),
+                name='W')
+        else:
+            self.W = W
+
+        if b is None:
+            self.b = theano.shared(
+                value=numpy.zeros((n_out,), dtype=theano.config.floatX),
+                name='b')
+        else:
+            self.b = b
+
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+
+
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+
+        self.params = [self.W, self.b]
+
+    def negative_log_likelihood(self, y):
+
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+
+    def errors(self, y):
+
+
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError('y should have the same shape as self.y_pred',
+                            ('y', target.type, 'y_pred', self.y_pred.type))
+        if y.dtype.startswith('int'):
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
