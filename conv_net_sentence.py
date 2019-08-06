bc18822d2de25bb5deaa61c5eeb955bf8c49c18a
wjj <wangjiajie0326@163.com>
2019-08-06 08:37:29 +0800

    init

---
+++ conv_net_sentence.py
@@ -0,0 +1,24 @@
+# *_*coding:utf-8 *_*
+import cPickle
+import numpy as np
+from collections import defaultdict, OrderedDict
+import theano
+import theano.tensor as T
+import re
+import warnings
+import sys
+warnings.filterwarnings("ignore")   
+
+#different non-linearities
+def ReLU(x):
+    y = T.maximum(0.0, x)
+    return(y)
+def Sigmoid(x):
+    y = T.nnet.sigmoid(x)
+    return(y)
+def Tanh(x):
+    y = T.tanh(x)
+    return(y)
+def Iden(x):
+    y = x
+    return(y)
