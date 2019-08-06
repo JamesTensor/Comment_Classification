6b9ba095383b39bd58ac2648614b621f75b454ea
wjj <wangjiajie0326@163.com>
2019-08-06 08:40:49 +0800

    创建train_conv_net class

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -7,7 +7,7 @@ import theano.tensor as T
 import re
 import warnings
 import sys
-warnings.filterwarnings("ignore")   
+warnings.filterwarnings("ignore")
 
 #different non-linearities
 def ReLU(x):
@@ -22,3 +22,33 @@ def Tanh(x):
 def Iden(x):
     y = x
     return(y)
+
+def train_conv_net(datasets,
+                   U,
+                   img_w=300,
+                   filter_hs=[3,4,5],
+                   hidden_units=[100,2],
+                   dropout_rate=[0.5],
+                   shuffle_batch=True,
+                   n_epochs=25,
+                   batch_size=50,
+                   lr_decay = 0.95,
+                   conv_non_linear="relu",
+                   use_valid_set=True,
+                   activations=[Iden],
+                   sqr_norm_lim=9,
+                   non_static=True):
+    rng = np.random.RandomState(3435)
+    img_h = len(datasets[0][0]) - 1
+    filter_w = img_w
+    feature_maps = hidden_units[0]
+    filter_shapes = []
+    pool_sizes = []
+    for filter_h in filter_hs:
+        filter_shapes.append((feature_maps, 1, filter_h, filter_w))
+        pool_sizes.append((img_h - filter_h + 1, img_w - filter_w + 1))
+    parameters = [("image shape", img_h, img_w), ("filter shape", filter_shapes), ("hidden_units", hidden_units),
+                  ("dropout", dropout_rate), ("batch_size", batch_size), ("non_static", non_static),
+                  ("learn_decay", lr_decay), ("conv_non_linear", conv_non_linear), ("non_static", non_static)
+        , ("sqr_norm_lim", sqr_norm_lim), ("shuffle_batch", shuffle_batch)]
+    print (parameters)
