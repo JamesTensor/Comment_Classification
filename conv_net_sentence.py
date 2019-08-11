37d02ee298a63e053dd13fc91e5b18e217eb162e
wjj <wangjiajie0326@163.com>
2019-08-11 15:03:43 +0800

    打乱并切分数据集

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -83,3 +83,44 @@ def train_conv_net(datasets,
     cost = classifier.negative_log_likelihood(y)
     dropout_cost = classifier.dropout_negative_log_likelihood(y)
     grad_updates = sgd_updates_adadelta(params, dropout_cost, lr_decay, 1e-6, sqr_norm_lim)
+
+    np.random.seed(3435)
+    if datasets[0].shape[0] % batch_size > 0:
+        extra_data_num = batch_size - datasets[0].shape[0] % batch_size
+        train_set = np.random.permutation(datasets[0])
+        extra_data = train_set[:extra_data_num]
+        new_data = np.append(datasets[0], extra_data, axis=0)
+    else:
+        new_data = datasets[0]
+    new_data = np.random.permutation(new_data)
+    n_batches = new_data.shape[0] / batch_size
+    n_train_batches = int(np.round(n_batches * 0.9))
+    if len(datasets) == 3:
+        use_valid_set = True
+        train_set = new_data
+        val_set = datasets[1]
+        train_set_x, train_set_y = shared_dataset((train_set[:, :img_h], train_set[:, -1]))
+        val_set_x, val_set_y = shared_dataset((val_set[:, :img_h], val_set[:, -1]))
+        test_set_x = datasets[2][:, :img_h]
+        test_set_y = np.asarray(datasets[2][:, -1], "int32")
+        n_val_batches = int(val_set.shape[0] / batch_size)
+        val_model = theano.function([index], classifier.errors(y),
+                                    givens={
+                                        x: val_set_x[index * batch_size: (index + 1) * batch_size],
+                                        y: val_set_y[index * batch_size: (index + 1) * batch_size]})
+    else:
+        test_set_x = datasets[1][:, :img_h]
+        test_set_y = np.asarray(datasets[1][:, -1], "int32")
+        if use_valid_set:
+            train_set = new_data[:n_train_batches * batch_size, :]
+            val_set = new_data[n_train_batches * batch_size:, :]
+            train_set_x, train_set_y = shared_dataset((train_set[:, :img_h], train_set[:, -1]))
+            val_set_x, val_set_y = shared_dataset((val_set[:, :img_h], val_set[:, -1]))
+            n_val_batches = n_batches - n_train_batches
+            val_model = theano.function([index], classifier.errors(y),
+                                        givens={
+                                            x: val_set_x[index * batch_size: (index + 1) * batch_size],
+                                            y: val_set_y[index * batch_size: (index + 1) * batch_size]})
+        else:
+            train_set = new_data[:, :]
+            train_set_x, train_set_y = shared_dataset((train_set[:, :img_h], train_set[:, -1]))  
