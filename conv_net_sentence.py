4ef45d1c15e1644d669b94814560b0fa6be7927f
wjj <wangjiajie0326@163.com>
2019-08-15 10:05:17 +0800

    加载数据供其他函数调用

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -168,3 +168,43 @@ def train_conv_net(datasets,
                 test_loss = test_model_all(test_set_x, test_set_y)
                 test_perf = 1 - test_loss
         return test_perf
+
+    def shared_dataset(data_xy, borrow=True):
+
+        data_x, data_y = data_xy
+        shared_x = theano.shared(np.asarray(data_x,
+                                            dtype=theano.config.floatX),
+                                 borrow=borrow)
+        shared_y = theano.shared(np.asarray(data_y,
+                                            dtype=theano.config.floatX),
+                                 borrow=borrow)
+        return shared_x, T.cast(shared_y, 'int32')
+
+    def sgd_updates_adadelta(params, cost, rho=0.95, epsilon=1e-6, norm_lim=9, word_vec_name='Words'):
+        
+        updates = OrderedDict({})
+        exp_sqr_grads = OrderedDict({})
+        exp_sqr_ups = OrderedDict({})
+        gparams = []
+        for param in params:
+            empty = np.zeros_like(param.get_value())
+            exp_sqr_grads[param] = theano.shared(value=as_floatX(empty), name="exp_grad_%s" % param.name)
+            gp = T.grad(cost, param)
+            exp_sqr_ups[param] = theano.shared(value=as_floatX(empty), name="exp_grad_%s" % param.name)
+            gparams.append(gp)
+        for param, gp in zip(params, gparams):
+            exp_sg = exp_sqr_grads[param]
+            exp_su = exp_sqr_ups[param]
+            up_exp_sg = rho * exp_sg + (1 - rho) * T.sqr(gp)
+            updates[exp_sg] = up_exp_sg
+            step = -(T.sqrt(exp_su + epsilon) / T.sqrt(up_exp_sg + epsilon)) * gp
+            updates[exp_su] = rho * exp_su + (1 - rho) * T.sqr(step)
+            stepped_param = param + step
+            if (param.get_value(borrow=True).ndim == 2) and (param.name != 'Words'):
+                col_norms = T.sqrt(T.sum(T.sqr(stepped_param), axis=0))
+                desired_norms = T.clip(col_norms, 0, T.sqrt(norm_lim))
+                scale = desired_norms / (1e-7 + col_norms)
+                updates[param] = stepped_param * scale
+            else:
+                updates[param] = stepped_param
+        return updates
