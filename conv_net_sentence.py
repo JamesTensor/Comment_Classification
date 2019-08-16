7bbc7a899e6faab104dc788d5986903c23ed550c
wjj <wangjiajie0326@163.com>
2019-08-16 09:40:08 +0800

    完善main

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -242,3 +242,60 @@ def train_conv_net(datasets,
         while len(x) < max_l + 2 * pad:
             x.append(0)
         return x
+
+    def make_idx_data_cv(revs, word_idx_map, cv, max_l=51, k=300, filter_h=5):
+        """
+        Transforms sentences into a 2-d matrix.
+        """
+        train, test = [], []
+        for rev in revs:
+            sent = get_idx_from_sent(rev["text"], word_idx_map, max_l, k, filter_h)
+            sent.append(rev["y"])
+            if rev["split"] == cv:
+                test.append(sent)
+            else:
+                train.append(sent)
+        train = np.array(train, dtype="int")
+        test = np.array(test, dtype="int")
+        return [train, test]
+
+    if __name__ == "__main__":
+
+        x = cPickle.load(open("mr.p", "rb"))
+        revs, W, W2, word_idx_map, vocab = x[0], x[1], x[2], x[3], x[4]
+
+        mode = sys.argv[1]
+        word_vectors = sys.argv[2]
+        if mode == "-nonstatic":
+            print("model architecture: CNN-non-static"
+            non_static = True
+        elif mode == "-static":
+            print("model architecture: CNN-static")
+            non_static = False
+        execfile("conv_net_classes.py")
+        if word_vectors == "-rand":
+            print("using: random vectors")
+            U = W2
+        elif word_vectors == "-word2vec":
+            print("using: word2vec vectors")
+            U = W
+        results = []
+        r = range(0, 10)
+        for i in r:
+            datasets = make_idx_data_cv(revs, word_idx_map, i, max_l=56, k=300, filter_h=5)
+            perf = train_conv_net(datasets,
+                                  U,
+                                  lr_decay=0.95,
+                                  filter_hs=[3, 4, 5],
+                                  conv_non_linear="relu",
+                                  hidden_units=[100, 2],
+                                  use_valid_set=True,
+                                  shuffle_batch=True,
+                                  n_epochs=25,
+                                  sqr_norm_lim=9,
+                                  non_static=non_static,
+                                  batch_size=50,
+                                  dropout_rate=[0.5])
+            print("cv: " + str(i) + ", perf: " + str(perf))
+            results.append(perf)
+        print(str(np.mean(results)))
