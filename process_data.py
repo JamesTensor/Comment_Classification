3e6219fa99cce17d8ea2710136f95fd2abe25215
wjj <wangjiajie0326@163.com>
2019-08-19 17:13:33 +0800

    Loads data and split into 10 folds.

---
+++ process_data.py
@@ -0,0 +1,30 @@
+# *_*coding:utf-8 *_*
+import numpy as np
+import cPickle
+from collections import defaultdict
+import sys, re
+import pandas as pd
+
+def build_data_cv(data_folder, cv=10, clean_string=True):
+
+    revs = []
+    pos_file = data_folder[0]
+    neg_file = data_folder[1]
+    vocab = defaultdict(float)
+    with open(pos_file, "rb") as f:
+        for line in f:
+            rev = []
+            rev.append(line.strip())
+            if clean_string:
+                orig_rev = clean_str(" ".join(rev))
+            else:
+                orig_rev = " ".join(rev).lower()
+            words = set(orig_rev.split())
+            for word in words:
+                vocab[word] += 1
+            datum  = {"y":1,
+                      "text": orig_rev,
+                      "num_words": len(orig_rev.split()),
+                      "split": np.random.randint(0,cv)}
+            revs.append(datum)
+    
